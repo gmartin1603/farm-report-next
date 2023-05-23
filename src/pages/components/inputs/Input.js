1f14f26d96db57d677c3b554bdc0bec956e01fb7
George <duramaxer228@gmail.com>
2023-05-23 10:46:14 -0500

    Added "row" display mode

--- src/pages/components/inputs/Input.js
+++ src/pages/components/inputs/Input.js
@@ -1,24 +1,31 @@
-import React from "react"
+import React from "react";
 
 const Input = (props) => {
-
-    const styles = {
-        main:`w-full flex flex-col items-start justify-start`,
-        input:`w-full text-black font-bold border border-black rounded p-1`,
-    }
+  const styles = {
+    main: `w-full flex ${
+      props.row ? "items-center" : "flex-col items-start"
+    } justify-start`,
+    input: `text-black border border-black rounded ${
+      props.row ? "text-center max-w-[65px]" : "w-full font-bold"
+    }`,
+    label: `w-max text-black font-bold mx-2`,
+  };
 
   return (
     <div className={styles.main}>
-        <label htmlFor={props.id}>{props.label}</label>
-        <input
+      <label className={styles.label} htmlFor={props.id}>
+        {props.label}
+      </label>
+      <input
         type={props.type}
         id={props.id}
+        placeholder={props.placeholder}
         className={styles.input}
         onChange={(e) => props.handleChange(e)}
         value={props.value}
-        />
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
